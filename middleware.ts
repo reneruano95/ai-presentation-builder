abfbdb8c236d26fcfda1500bcc82978dbdda29c1
Rene Ruano <rene.ruano95@gmail.com>
2025-03-31 21:53:24 -0400

    feat: add authentication layout and sign-in/sign-up pages

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   // Check if the request is for a public route and if so, skip authentication
